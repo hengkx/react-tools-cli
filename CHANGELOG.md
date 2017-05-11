54cfc51e0028b239ae3e2aadf08503570a7e28d7
hengkx <ycxzhkx@gmail.com>
2017-05-11 18:14:31 +0800

    watch saga finish

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.3.0
+1. add watch saga,auto create 'saga/index' and 'reducer/index' file
+
+# 0.2.0
+1. add create action,reducer,saga
--- src/watch.js
+++ src/watch.js
@@ -3,33 +3,74 @@ import fs from 'fs-extra';
 import colors from 'colors';
 import chokidar from 'chokidar';
 import omit from 'object.omit';
+import chalk from 'chalk';
 import _ from 'lodash';
 import { getWatchSagas, getActions } from './utils/saga';
 
+function handleSagaChange(saga, sagaIndexPath, reducerIndexPath) {
+  if (saga) {
+    let str = '';
+    let reducerStr = `import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import { nprogress } from 'redux-nprogress';
+`;
+    const allKey = [];
+    const allSaga = [];
+    Object.keys(saga).forEach(key => {
+      const content = saga[key];
+      if (content) {
+        const sagas = _.map(content, 'name');
+        allSaga.push(...sagas);
+        if (sagas.length > 0) {
+          allKey.push(key);
+          reducerStr += `import ${key} from '../sagas/${key}';\n`;
+          str += `import { ${sagas.join(', ')} } from './${key}';\n`;
+        }
+      }
+    });
+    str += `\nexport default function* rootSaga() {
+  yield [
+    ${allSaga.join('(),\n    ')}()\n  ];
+}`;
+    fs.writeFileSync(sagaIndexPath, str);
+    reducerStr += `\nexport default combineReducers({
+  routing: routerReducer,
+  nprogress,
+  ${allKey.join(',\n  ')}\n});`;
+    fs.writeFileSync(reducerIndexPath, reducerStr);
+    console.log(chalk.green.bold('更新成功！'));
+  }
+}
+
+
 function watch(program, { cwd }) {
   const sagaPath = path.join(cwd, 'src', 'sagas');
-
+  if (!fs.existsSync(sagaPath)) fs.mkdirsSync(sagaPath);
+  const sagaIndexPath = path.join(sagaPath, "index.js");
+  const reducerPath = path.join(cwd, 'src', 'reducers');
+  if (!fs.existsSync(reducerPath)) fs.mkdirsSync(reducerPath);
+  const reducerIndexPath = path.join(reducerPath, 'index.js');
   let sagas = {};
-
+  let isInitFinished = false;
   const listenPath = `${sagaPath}/**/*.js`;
   chokidar.watch(listenPath,
-    { ignored: 'index.js' })
+    { ignored: /index.js/ })
     .on('all', (event, filepath) => {
+      const name = path.basename(filepath).replace(".js", "");
       if (event === 'unlink') {
-        sagas = omit(sagas, path.basename(filepath));
-        console.log(sagas);
+        sagas = omit(sagas, name);
       } else {
-        fs.readFile(filepath, 'utf-8', function (err, data) {
-          if (err) {
-            console.log("error");
-          } else {
-            sagas[path.basename(filepath)] = getWatchSagas(data);
-            console.log(sagas);
-          }
-        });
+        const data = fs.readFileSync(filepath, 'utf-8');
+        sagas[name] = getWatchSagas(data);
+      }
+      if (isInitFinished) {
+        handleSagaChange(sagas, sagaIndexPath, reducerIndexPath);
       }
     })
-    .on('ready', () => console.log('Initial scan complete. Ready for changes'))
+    .on('ready', () => {
+      isInitFinished = true;
+      handleSagaChange(sagas, sagaIndexPath, reducerIndexPath);
+    })
     .on('error', error => console.log(`Watcher error: ${error}`));
 }
 
